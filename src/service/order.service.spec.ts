8e6dfb2f9fe762ad207722d01b1d6c5b032c5dd6
victorhugoaraujo <victorhugo_araujo@hotmail.com>
2022-11-22 15:47:10 -0300

    Add customer unit test add rewardPoints

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,22 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/orderitem"
 import OrderService from "./order.service";
 
 describe("Order Service unit tests", () => {
+
+  it("should place and order", () => {
+
+    const customer = new Customer("c1", "Customer 1");
+    const item1 = new OrderItem("i1", "Item 1", 10, "p1", 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10)
+  })
+
+
   it("should get total of all orders", () => {
     const item1 = new OrderItem("i1", "Item 1", 100, "p1", 1);
     const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
