a4ea14f0b693af6dd3c7e8b645f5186356743d18
victorhugoaraujo <victorhugo_araujo@hotmail.com>
2022-11-25 15:33:19 -0300

    order repository implements order-repository-interface

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,8 +1,9 @@
 import Order from "../../domain/entity/order";
+import OrderRepositoryInterface from "../../domain/repository/order-repository-interface";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create({
       id: entity.id,
@@ -18,6 +19,18 @@ export default class OrderRepository {
     },
       {
         include: [{ model: OrderItemModel }],
-      })
-  }
+      });
+  };
+
+  async update(entity: Order): Promise<void> {
+    throw new Error("not implemented")
+  };
+
+  async find(id: string): Promise<Order> {
+    throw new Error("not implemented")
+  };
+
+  async findAll(): Promise<Order[]> {
+    throw new Error("not implemented")
+  };
 }
