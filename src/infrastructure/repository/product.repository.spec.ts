4588e67bf1eb1a74aef9c7d8f91b8a26a817d2d8
victorhugoaraujo <victorhugo_araujo@hotmail.com>
2022-11-28 13:54:18 -0300

    Product and Customer unit test setup fix

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -9,7 +9,7 @@ describe("Product repository test", () => {
   beforeEach(async () => {
     sequelize = new Sequelize({
       dialect: "sqlite",
-      storage: ":memory",
+      storage: ":memory:",
       logging: false,
       sync: { force: true }
     });
